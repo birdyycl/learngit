71812405eda19d7378acf48587c447af4e3f3dca
YCL <804066757@qq.com>
2021-01-12 15:39:42 +0800

    append GPL

--- test_one.cpp
+++ test_one.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	cout << "Git is a version control system." << endl;
-	cout << "Git is free software." << endl
+	cout << "Git is a distributed version control system." << endl;
+	cout << "Git is free software distributed under the GPL." << endl
 	return 0;
 }
