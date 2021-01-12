35d59f7a5fc4741fbf2abf09121be94618c75e8b
YCL <804066757@qq.com>
2021-01-12 16:25:40 +0800

    understand how stage works

--- test_one.cpp
+++ test_one.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
 	cout << "Git is a distributed version control system." << endl;
-	cout << "Git is free software distributed under the GPL." << endl
+	cout << "Git is free software distributed under the GPL." << endl;
+	coud << "hello." <<endl;
 	return 0;
 }
